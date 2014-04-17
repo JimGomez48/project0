8e1c314895fb829262367e44af741cb634c20615
jimunoz1990 <jimunoz1990@gmail.com>
2014-04-17 13:30:14 -0700

    Update, not functional
    Updated some overhead stuff and currently working on copy numbers

--- 1million_random_seq.py
+++ 1million_random_seq.py
@@ -18,27 +18,38 @@ def insertToStr (arr, index, char):
     arr2 = arr[index:]
     return arr1+char+arr2
 
+def copyToStr (arr, index, size):
+    arr1=arr[index[0]:index[0]+size]
+    #copy the substring into the array
+    for i in range(1,len(index)):
+        arr[index[i]:index[i]+size]=arr1
+    return arr
+
 # Generate random genetic sequence of length 1 million
 nucleobaseList = ["C","T","G","A"]
 
 # This is the true file without error introduced
 baseFile = open("1_million_random_seq.txt", "w")
 
-# This is the true file with error introduced
-baseErrorFile = open("1_million_random_seq_error.txt", "w")
 counter = 0
 
-stringLen=100000000
+#This is where we will store the answer key, which includes:
+#ID
+#Copy numbers
+#Ins/Del
+#
+stringLen=1000000
+
+#create original string and ID
+#ID will be a hash of the first 100 characters
 
 for i in range(0, stringLen):
     randomBase = random.choice(nucleobaseList)
-
-    
     baseFile.write(randomBase)
     
     # Insert error bases (1% error) into an error file
     # Get value between 0 and 1
-    randomCondition = random.random()
+    '''randomCondition = random.random()
     if (randomCondition <= 0.01):
         randomBaseError = random.choice(nucleobaseList)
         while (randomBaseError == randomBase):
@@ -50,14 +61,71 @@ for i in range(0, stringLen):
         #if (randomBase == randomBaseError):
         #    print("ERROR")
     else:
-        baseErrorFile.write(randomBase)
-        
+        baseErrorFile.write(randomBase)'''
 baseFile.close()
-baseErrorFile.close()
+#baseErrorFile.close()
 
 # Open file for getting reads
-baseErrorFile = open("1_million_random_seq_error.txt", "r")
-baseFileList = baseErrorFile.readlines()
+baseFile = open("1_million_random_seq.txt", "r")
+
+baseFileList = baseFile.readline()
+
+ID = hash(baseFileList[:100])
+baseAnswerFile = open("1_million_random_seq_error_ans_key.txt", "w")
+baseAnswerFile.write("ID: ")
+baseAnswerFile.write(str(ID))
+baseAnswerFile.write("\n")
+
+baseAnswerFile.close()
+#Copy Numbers
+#Sequence of random length between 20-50
+'''
+for copyLoop in range(0,1):
+    print copyLoop
+    copyLen = random.randint(20,50)
+    copyIndex=[]
+    copyIndex.append(int(random.random()*(stringLen - copyLen) ))
+    
+    #0.001% of the time the string is copied
+    for i in range (0,(int)(stringLen*0.00001)):
+        temp=(int(random.random()*(stringLen - copyLen) ))
+        write=1
+        for j in range (0,len(copyIndex)-1):
+            if(temp>copyIndex[j] and temp<copyIndex[j]+copyLen):
+                write=0
+                break;
+        if(write==1):
+            copyIndex.append(temp)
+    
+    baseFileList=copyToStr(baseFileList,copyIndex,copyLen)
+    baseAnswerFile.write("Copy Num: \n")
+    baseAnswerFile.write(baseFileList[copyIndex:copyIndex+copyLen])
+    baseAnswerFile.write("\n")
+    baseAnswerFile.write(copyIndex)
+    baseAnswerFile.write("\n")
+
+baseAnswerFile.close()
+
+#sys.exit([0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # File to hold reads from 1 million char sequence
 readsFile = open("1_million_random_seq_error_reads.txt", "w")
@@ -121,4 +189,4 @@ for i in range(0, (int)(stringLen*0.15)):
     
 baseErrorFile.close()
 readsFile.close()    
-    
+    '''
